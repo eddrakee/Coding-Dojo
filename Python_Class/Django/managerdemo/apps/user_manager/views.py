ea21785a0febb9e02a78abdc5ddda3218f44a2b6
Elise Drake <edrake09@gmail.com>
2017-01-24 13:43:40 -0800

    initial commit for login_registration

--- Python_Class/Django/managerdemo/apps/user_manager/views.py
+++ Python_Class/Django/managerdemo/apps/user_manager/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import User
 # Create your views here.
 def index(request):
-    #at one point i had too few positional arguments for this and now i have enough! yay!!
-    #user property is an instance of our user manager class. user manager can use all manager properties
-    #it allows us to make customized queries. pretty cool. Kargs-keyWord arguments
+    #Make sure to have enough positional arguments!
+    #The user property is an instance of our UserManager class.
+    #UserManager can use all the manager properties and allows us to make customized queries.
+    #Kwargs = Key Word Arguments 
+    
     context={
         'all_users': User.objects.all()
     }
@@ -12,9 +14,10 @@ def index(request):
 
 def create_user(request):
     if request.method=='POST':
-        # request.POST == a dictionary
+        # request.POST is a dictionary
+        #this refers back to the class UserManager>def register>postData
         print (request.POST, "in create user, expect next thing to be in User.objects")
         User.objects.register(request.POST)
         #.register refers to the class method in userManager called register
-        # postData= User.objects.login(request.POST['password'], request.POST['first_name'], request.POST['last_name'])
+        # postData = User.objects.login(request.POST['password'], request.POST['first_name'], request.POST['last_name'])
         return redirect('/')
