2c99a07f7cd9481695b6961901504c51033a2adc
Elise Drake <edrake09@gmail.com>
2017-01-23 17:01:13 -0800

    finished working on courses_assignment

--- Python_Class/Django/courses_assignment/apps/courses/urls.py
+++ Python_Class/Django/courses_assignment/apps/courses/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^courses$', views.create_course),
+    url(r'^courses$', views.print_course),
+    url(r'^remove/(?P<id>\d+)$', views.delete),
 ]
--- Python_Class/Django/courses_assignment/apps/courses/views.py
+++ Python_Class/Django/courses_assignment/apps/courses/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from . models import Course, Description, Comment
+import re
 
 # Create your views here.
 def index(request):
-    return request('/')
+    print "show index.html"
+    all_the_courses = {
+        'all_courses': Course.objects.all(),
+    }
+    return render(request,'courses/index.html', all_the_courses)
+
+def create_course(request):
+    print "create_course"
+    error = 0
+   
+    course = Course.objects.create(course_name=request.POST['course_name'])
+    course_id=course.id #we made up course_id, it is taking in the id of the variable course that we just created
+    Description.objects.create(content=request.POST['description'], course_id=course_id)
+
+    return redirect('/')
+
+def print_course(request):
+    all_the_courses = {
+        'all_courses': Course.object.all(),
+    }
+
+    return render(request, '/courses/index.html', all_the_courses)
+
+def delete(request, id): #this id is referring to the id in our urls.py and the value comes from index.html on the form action
+    if request.method =="POST":
+        Course.objects.filter(id=id).delete() #the first id belongs to the Course.objects. The second id is the one being passed through the browser
+        Description.objects.filter(id=id).delete()
+    return redirect('/')
+
+
