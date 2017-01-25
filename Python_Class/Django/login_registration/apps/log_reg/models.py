903e480a7d5c54d79dbc1800832645c4339617ac
Elise Drake <edrake09@gmail.com>
2017-01-24 19:17:43 -0800

    finished login reg

--- Python_Class/Django/login_registration/apps/log_reg/models.py
+++ Python_Class/Django/login_registration/apps/log_reg/models.py
@@ -45,31 +45,37 @@ class UserManager(models.Manager): # This is exclusive to User class
         #we don't return anything since we are simply adding data to Users and don't require anything in return
     
     def login_user(self, postData):
-        user_info = User.objects.filter(email=postData['email']) #find the User row that contains the entered email
+        user_info = self.filter(email=postData['email']) #find the User row that contains the entered email
         if not user_info: #if user_info is false...
             return False #if the email isn't there, get outta here!
-        print ("first", user_info) #so we can see if it's the correct user we're talking about
+        print ('- First instance of user_info -')
+        print (user_info) #so we can see if it's the correct user we're talking about
         #user_info currently gives you an array which has a dictionary inside
         user_info = user_info[0] #traverse to the first index in the list
-        print ("second", user_info) #now we get object!
+        print ('- Second instance of user_info -')
+        print (user_info) #now we get object!
         pw_hash = user_info.password.encode() #this set pw_hash to equal the password for the user object we just printed in the terminal
         if pw_hash == bcrypt.hashpw(postData['password'].encode(), pw_hash): #the second pw_hash is being used to check if it matches the postData password
             return True
         return False #if it does not return True, it will return False which means the password doesn't match
-    
+
+        #this sets the session id
     def set_session(self, postData): #with this method, we will be retrieving an id and sending to our views.py
         #we already know that the inputed email exists, so now we want to find associated user info with the line below
         #Also, since we know that we will be getting a list with one item, we can skip setting this to find_user[0] and say .first() instead
-        find_user = User.objects.filter(email=postData['email']).first()
+        find_user = self.filter(email=postData['email']).first()
         return find_user.id #this sends returns the id value
-
-
-
+       
+        #this will get the session user data
+    def set_user_session(self, session_id):
+        #when you log in or register, an id for session gets created. We are now naming it session_id
+        find_session_user = self.get(id=session_id) #.get will give you one object
+        return find_session_user #to use this in my views.py, I must return it!
 
 
 #we migrate if we change anything in the User class
 class User(models.Model):
-        first_name = models.CharField(max_length=45)
+        first_name = models.CharField(max_length=45) #each field is an attribute
         last_name = models.CharField(max_length=45)
         email = models.CharField(max_length=100)
         password = models.CharField(max_length=100)
