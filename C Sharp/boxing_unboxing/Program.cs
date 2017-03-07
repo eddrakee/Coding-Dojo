e215b31b4f53d4f97df570786216cae27e37c2d5
Elise Drake <edrake09@gmail.com>
2017-03-06 16:12:08 -0800

    finished boxing_unboxing

--- C Sharp/boxing_unboxing/Program.cs
+++ C Sharp/boxing_unboxing/Program.cs
@@ -7,12 +7,25 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            // Create an empty List of type object
             List<object> listObj = new List<object>();
+
+            // Add these items to the list
             listObj.Add(7);
             listObj.Add(28);
             listObj.Add(-1);
             listObj.Add(true);
             listObj.Add("chair");
+
+            // Loop through the list and display all values and sum all integers
+            int sum = 0;
+            foreach(var obj in listObj){
+                System.Console.WriteLine(obj);
+                if(obj is int){
+                    sum += (int)obj;
+                }
+            }
+            System.Console.WriteLine("The sum of all integers is {0}", sum);
         }
     }
 }
