faf4543c7ece629d607f96ee948e216f34572993
Elise Drake <edrake09@gmail.com>
2017-03-28 13:12:41 -0700

    adding features

--- C Sharp/Belt_ActivityCenter/Controllers/DashController.cs
+++ C Sharp/Belt_ActivityCenter/Controllers/DashController.cs
@@ -30,12 +30,66 @@ namespace UserDash.Controllers
             List<User> AllUsers = _context.Users
                 .OrderBy(u => u.UserId)
                 .ToList();
-            // CurrentUser info is now in ViewBag
+            List<Activity> AllActivities = _context.Activity.ToList();
+            ViewBag.AllActivities = AllActivities;
             ViewBag.User = CurrentUser;
-            // AllUser info is now in ViewBag
             ViewBag.AllUsers = AllUsers;
-            return View("UserDashboard");
+
+            return View("Dashboard");
         }
-       
+        // ADD ACTIVITY - GET
+        [HttpGet]
+        [RouteAttribute("AddActivity")]
+        public IActionResult AddActivity()
+        {
+            User CurrentUser = _context.Users.Where(u => u.UserId == (int)HttpContext.Session.GetInt32("UserId")).SingleOrDefault();
+            ViewBag.User = CurrentUser;
+            return View("AddActivity");
+        }
+        // ADD ACTIVITY - POST
+        [HttpPost]
+        [RouteAttribute("AddActivity")]
+        public IActionResult AddActivity(Activity NewActivity)
+        {
+            User CurrentUser = _context.Users.Where(u => u.UserId == (int)HttpContext.Session.GetInt32("UserId")).SingleOrDefault();
+            if (ModelState.IsValid)
+            {
+                NewActivity.ActivityCreatorId = CurrentUser.UserId;
+                NewActivity.ActivityJoinerId = CurrentUser.UserId;
+                NewActivity.CreatedAt = DateTime.Now;
+                NewActivity.UpdatedAt = DateTime.Now;
+                _context.Add(NewActivity);
+                _context.SaveChanges();
+                Activity JustAdded = _context.Activity.Where( i => i.ActivityId == NewActivity.ActivityId).SingleOrDefault();  
+                return RedirectToAction("ShowOne", new {ActivityId = NewActivity.ActivityId});
+            }
+            else
+            {
+                ViewBag.AllErrors = ModelState.Values;
+                return View("AddActivity");
+            }
+        }
+        // SHOW ONE ACTIVITY - GET
+        [HttpGet]
+        [RouteAttribute("ShowOne/{ActivityId}")]
+        public IActionResult ShowOne(int ActivityId)
+        {
+            
+            Activity ActivityProfile = _context.Activity.SingleOrDefault( i => i.ActivityId == ActivityId);
+            User CurrentUser = _context.Users.Where(u => u.UserId == (int)HttpContext.Session.GetInt32("UserId"));
+            ViewBag.User = CurrentUser;
+            ViewBag.Profile = ActivityProfile;
+            return View("ShowOne");
+        }
+        // DELETE ACTIVITY - GET
+        [HttpGet]
+        [RouteAttribute("Delete/{ActivityId}")]
+        public IActionResult Delete(int ActivityId)
+        {
+            Activity DeleteAct = _context.Activity.SingleOrDefault( d => d.ActivityId == ActivityId);
+            _context.Activity.Remove(DeleteAct);
+            _context.SaveChanges();
+            return RedirectToAction("Dashboard");
+        }
     }
 }
